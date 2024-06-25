98312da274ce867b22dd7bee27872d54d592e41e
gevorggasparyan <gevorg.gasparyan@esterox.am>
2024-06-25 12:53:33 +0400

    updated put method

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const validateBalance = require("../middleware/validateBalance");
 
-router.post("/update-balance", validateBalance, userController.updateBalance);
+router.put("/update-balance", validateBalance, userController.updateBalance);
 router.get("/balance/:userId", userController.getBalance);
 
 module.exports = router;
